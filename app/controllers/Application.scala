0bf273d4b15a8b78ce1d166189f7a416cace4782
Drew Roby (STL) <drew.roby@daugherty.com>
2023-02-05 19:21:26 -0600

    routing details 2:18

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -8,6 +8,7 @@ import javax.inject._
 @Singleton
 class Application @Inject() (cc: ControllerComponents) extends AbstractController(cc) {
 
-  def index = TODO //Action {
-//    Ok(views.html.index(SharedMessages.itWorks))
+  def index = Action {
+    Ok(views.html.index())
+  }
 }
--- app/controllers/TaskList1.scala
+++ app/controllers/TaskList1.scala
@@ -9,7 +9,12 @@ import javax.inject._
 class TaskList1 @Inject() (cc: ControllerComponents) extends AbstractController(cc) {
 
   def taskList = Action {
-    Ok("This works!")
+    val tasks = List("task1","task2","task3","sleep","eat")
+    Ok(views.html.taskList1(tasks))
+  }
+
+  def product(prodType: String, prodNum: Int) = Action {
+    Ok(s"Product type is : $prodType, product number is: $prodNum")
   }
 
 }
